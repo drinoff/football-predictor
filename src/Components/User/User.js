392597be50739a4e0ff882301c007892a9b1014d
Drinoff <drinoff@gmail.com>
2021-11-08 22:36:49 +0100

    User component styling

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -9,8 +9,8 @@ const User = () => {
                 src="https://firebasestorage.googleapis.com/v0/b/football-predictor-3416d.appspot.com/o/Avatars%2Fdrinoff.jpg?alt=media&token=0450cfe6-95d4-41f4-a311-35136b2e54af"
                 sx={{ width: "54px", height: "54px" }}
             />
-            <a href="http://">Профил</a>
-            <a href="http://">Мои Прогнози</a>
+            <a href="http://">My Profile</a>
+            <a href="http://">My Predictions</a>
         </div>
     );
 };
