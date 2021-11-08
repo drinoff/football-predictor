a68db9cb259ead9500bf1b02e40fa2439adb5d23
Drinoff <drinoff@gmail.com>
2021-11-08 22:37:25 +0100

    React Router installed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
+
 import Header from"./Components/Header/Header";
+import Matches from "./Components/Matches/Matches";
 
 function App() {
     return (
     <div className = "App" >
 
         <Header />
+
+        <Routes>
+        <Route path="/matches" element={<Matches />} />
+        
+      </Routes>
     </div>)
 };
 
