3cd64ece3dd56d119f52c31be465cd52f8f5cd6c
Drinoff <drinoff@gmail.com>
2021-11-08 21:53:26 +0100

    added avatar component from mui

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,19 +1,18 @@
-import { Avatar } from '@mui/material';
-import { deepOrange } from '@mui/material/colors';
+import { Avatar } from "@mui/material";
 import "./User.css";
 
 const User = () => {
-  return (
-    <div className = "userHeaderCard">
-      <Avatar 
-      alt="Drin Drinoff" 
-      src="./drinoff.jpg" 
-      sx={{ width: 54, height: 54, backgroundColor: "#1F6CFA" }}
-      />
-      <a href="http://">Профил</a>
-      <a href="http://">Мои Прогнози</a>
-    </div>
-  );
-}
+    return (
+        <div className="userHeaderCard">
+            <Avatar
+                alt="Drin Drinoff"
+                src="https://firebasestorage.googleapis.com/v0/b/football-predictor-3416d.appspot.com/o/Avatars%2Fdrinoff.jpg?alt=media&token=0450cfe6-95d4-41f4-a311-35136b2e54af"
+                sx={{ width: "54px", height: "54px" }}
+            />
+            <a href="http://">Профил</a>
+            <a href="http://">Мои Прогнози</a>
+        </div>
+    );
+};
 
 export default User;
