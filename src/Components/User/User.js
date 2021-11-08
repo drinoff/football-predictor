e82816631771419d81ae10031024c6f075f8c9dd
Drinoff <drinoff@gmail.com>
2021-11-08 20:30:25 +0100

    user card styling

--- src/Components/User/User.js
+++ src/Components/User/User.js
@@ -1,13 +1,14 @@
 import { Avatar } from '@mui/material';
+import { deepOrange } from '@mui/material/colors';
 import "./User.css";
 
 const User = () => {
   return (
     <div className = "userHeaderCard">
       <Avatar 
-      alt="Mehmet Ismail" 
-      src="../../Assets/images/drinoff.jpg" 
-      sx={{ width: 54, height: 54 }}
+      alt="Drin Drinoff" 
+      src="./drinoff.jpg" 
+      sx={{ width: 54, height: 54, backgroundColor: "#1F6CFA" }}
       />
       <a href="http://">Профил</a>
       <a href="http://">Мои Прогнози</a>
