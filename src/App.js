4f6d7eeaab3874e7ab9abcf72fbed648f0669805
Drinoff <drinoff@gmail.com>
2021-11-10 22:18:49 +0100

    new routes added

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Header />
 
         <Routes>
-        <Route path="/matches" element={<Matches />} />
+        <Route path="/" element={<Matches />} />
         
       </Routes>
     </div>)
--- src/Components/Header/NavBar/NavBar.js
+++ src/Components/Header/NavBar/NavBar.js
@@ -5,7 +5,7 @@ import "./NavBar.css";
 const NavBar = () => {
     return (
         <ul className = 'navigation'>
-            <Link to='matches' className='li'>Matches</Link>
+            <Link to='/' className='li'>Matches</Link>
             <Link to='predictions' className='li'>Predictions</Link>
             <Link to='about' className='li'>About</Link>
             <Link to='contact' className = "contact li">Contact</Link>
