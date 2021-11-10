5afe4d9713e34761a421fc22ddd71e0098f17f38
Drinoff <drinoff@gmail.com>
2021-11-10 23:48:25 +0100

    MatchItem styling

--- src/Components/Matches/MatchItem.js
+++ src/Components/Matches/MatchItem.js
@@ -3,6 +3,7 @@ import "./MatchItem.css";
 const MatchItem = ({ match }) => {
     return (
         <div className="singleMatchContainer">
+            <p className = 'koTime'>{match.kickOffTime}</p>
             <p className="homeTeam">{match.homeTeam}</p>
             <div className = 'scoreWrapper'>
                 <p className="homeTeamScore">{match.currentHomeScore}</p> :
