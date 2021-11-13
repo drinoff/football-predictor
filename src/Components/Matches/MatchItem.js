02a702bc5ec1aca3290a755718e270233dd5e708
Drinoff <drinoff@gmail.com>
2021-11-13 15:30:43 +0100

    Matsch Item implemented with single match

--- src/Components/Matches/MatchItem.js
+++ src/Components/Matches/MatchItem.js
@@ -1,15 +1,28 @@
 import "./MatchItem.css";
 
-const MatchItem = ({ match }) => {
+const MatchItem = ({ match, onClick }) => {
     return (
-        <div className="singleMatchContainer">
-            <p className = 'koTime'>{match.kickOffTime}</p>
-            <p className="homeTeam">{match.homeTeam}</p>
-            <div className = 'scoreWrapper'>
-                <p className="homeTeamScore">{match.currentHomeScore}</p> :
-                <p className="awayTeamScore">{match.currentAwayScore}</p>
+        <div
+            onClick={() => onClick(match.fixture.id)}
+            className="singleMatchContainer"
+        >
+            <img
+                className="nationFlag"
+                src={match.league.flag}
+                alt={match.league.name}
+            />
+            <p className="leagueName">{match.league.country}</p>
+            <p className="matchStatus">{match.fixture.status.short}</p>
+
+            <div className="teamResultWrapper">
+                <p className="homeTeam">{match.teams.home.name}</p>
+                <div className="scoreWrapper">
+                    <p className="homeTeamScore">{match.score.fulltime.home}</p>
+                    :
+                    <p className="awayTeamScore">{match.score.fulltime.away}</p>
+                </div>
+                <p className="awayTeam">{match.teams.away.name}</p>
             </div>
-            <p className="awayTeam">{match.awayTeam}</p>
         </div>
     );
 };
