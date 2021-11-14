893bd60f9211010410c8b5524ba36e33563fcba3
Drinoff <drinoff@gmail.com>
2021-11-14 06:58:53 +0100

    netlify functions implemented

---
+++ functions/fetch.js
@@ -0,0 +1,26 @@
+import {date} from './todayDate';
+const fetch = require("node-fetch");
+
+
+const REACT_APP_FOOTBALL_API_KEY = process.env.FOOTBALL_API_KEY;
+const tzUser = 'Europe/London';
+
+const API_ENDPOINT = `https://v3.football.api-sports.io/fixtures?date=${date}&timezone=${tzUser}`;
+
+exports.handler = async (event, context) => {
+  try {
+    const response = await fetch(API_ENDPOINT,{
+        headers: {
+            "x-apisports-key": REACT_APP_FOOTBALL_API_KEY,
+        }
+    });
+    const data = await response.json();
+    return { statusCode: 200, body: JSON.stringify({ data }) };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed fetching data' }),
+    };
+  }
+};
