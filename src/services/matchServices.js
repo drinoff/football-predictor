728faae43308a820229760674d8e741a9019a776
Drinoff <drinoff@gmail.com>
2021-11-18 00:38:41 +0100

    updated api key request

--- src/services/matchServices.js
+++ src/services/matchServices.js
@@ -7,7 +7,7 @@ const BASE_URL = 'https://v3.football.api-sports.io/';
 const getAllMatches = ()=>{
     return fetch(`${BASE_URL}fixtures?date=${todaysDate()}&timezone=${tzUser}`,{
         headers: {
-            "x-apisports-key": API_KEY,
+            "x-apisports-key": process.env.REACT_APP_FOOTBALL_API_KEY,
         }
     })
     .then((res) => res.json())
@@ -16,7 +16,7 @@ const getAllMatches = ()=>{
 const getH2H = (homeId,awayId)=>{
     return fetch(`${BASE_URL}fixtures/headtohead?h2h=${homeId}-${awayId}`,{
         headers: {
-            "x-apisports-key": API_KEY,
+            "x-apisports-key": process.env.REACT_APP_FOOTBALL_API_KEY,
         }
     })
     .then((res) => res.json())
