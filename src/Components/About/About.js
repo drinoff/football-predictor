db8663d1e75e6859480122a1faef3d814543c163
Drinoff <drinoff@gmail.com>
2021-11-18 23:06:46 +0100

    created About Component

---
+++ src/Components/About/About.js
@@ -0,0 +1,22 @@
+const About = () => {
+    return (
+        <div className="about">
+            <h1>Football Predictor</h1>
+            <p>
+                We are a football enthusiasts, who mainly watch football in our
+                Free time. We love football, we dream about football, and of
+                course we decided why not materialize our passion. We decided to
+                create a platform where we can share our passion with the
+                world.We created this platform to help you find the best
+                football bets and stats. With our platform you can share your bets as well.
+
+                This platform is made purely for fun. Please be careful while betting, since it can be addictive.
+                Betting is a serious business, and we are not responsible for any losses.
+                We hope you enjoy our platform.
+                
+            </p>
+        </div>
+    );
+};
+
+export default About;
