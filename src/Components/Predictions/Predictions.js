64b2b04beaea193d9d122bf832c4bb7abaf79ee2
Drinoff <drinoff@gmail.com>
2021-11-24 22:59:39 +0100

    predictions page initial commit

--- src/Components/Predictions/Predictions.js
+++ src/Components/Predictions/Predictions.js
@@ -1,6 +1,18 @@
+import {useEffect, useState} from 'react';
+
+import predictionServices from '../../services/predictionServices';
+
 import Prediction from "./Prediction/Prediction";
 
 const Predictions = (props) => {
+    const [predictions, setPredictions] = useState();
+    
+    useEffect(() => {
+        predictionServices.getPredictions()
+        .then(data=>{setPredictions(data)
+            console.log(data)
+        })
+    },[]);
     return (
     <>
         {props.predictions
