f756e10d2909771358b5636784ae1539110901a0
Drinoff <drinoff@gmail.com>
2021-12-01 19:54:49 +0100

    created register user in authServices

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,13 +1,18 @@
 import '../utils/firebase';
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
 const auth = getAuth();
 
 const login = (email, password) => {
    return signInWithEmailAndPassword(auth, email, password);
 };
 
+const register = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);  
+}
+
 const authServices = {
     login,
+    register
 };
 
 
