72fee8eb896a926e58cd2723bbbbf3ad03422086
Drinoff <drinoff@gmail.com>
2021-12-02 19:53:46 +0100

    MyPrediction component initial commit

---
+++ src/Components/MyPredictions/MyPredictions.js
@@ -0,0 +1,8 @@
+
+const MyPredictions = () => {
+    return (
+        <div>usaaaa</div>
+    );
+};
+
+export default MyPredictions;
--- src/Components/Predictions/Predictions.js
+++ src/Components/Predictions/Predictions.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
+
 import Prediction from "./Prediction/Prediction";
 
 import predictionServices from "../../services/predictionServices";
