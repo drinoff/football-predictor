913778f53fe11f199a5219bdbfc6e04080f19a90
Drinoff <drinoff@gmail.com>
2021-12-03 19:08:34 +0100

    myPredicition component created

--- src/Components/MyPredictions/MyPredictions.js
+++ src/Components/MyPredictions/MyPredictions.js
@@ -1,7 +1,51 @@
+import { useState, useEffect, useContext } from "react";
+
+import Prediction from "../Predictions/Prediction/Prediction";
+
+import AuthContext from "../../contexts/AuthContext";
+import predictionServices from "../../services/predictionServices";
+import matchServices from "../../services/matchServices";
+import getStatus from "../../utils/getStatus";
+
+import './MyPredictions.css'
 
 const MyPredictions = () => {
+    const [userPredictions, setUserPredictions] = useState([]);
+    const { user } = useContext(AuthContext);
+
+    useEffect(() => {
+        predictionServices.getPredictions().then((res) => {
+            let data = Object.entries(res);
+            data.forEach((prediction) => {
+                matchServices
+                    .getMatchById(prediction[1].matchDetails.fixture.id)
+                    .then((matchUpdate) => {
+                        prediction[1].matchDetails.fixture.status.short =
+                            matchUpdate.response[0].fixture.status.short;
+                        prediction[1].matchDetails.predictionStatus = getStatus(
+                            matchUpdate,
+                            prediction[1].prediction
+                        );
+                    });
+            });
+            setTimeout(() => {
+            setUserPredictions(
+                data.filter((prediction) => prediction[1].email === user.email)
+            );
+            }, 2000);
+        });
+    }, [user]);
+
     return (
-        <div>usaaaa</div>
+        <div className = 'myPredictionsContainer'>
+            {userPredictions.map((prediction) => (
+                <Prediction
+                    key={prediction[0]}
+                    matchInfo={prediction[1]}
+                    isRender
+                />
+            ))}
+        </div>
     );
 };
 
