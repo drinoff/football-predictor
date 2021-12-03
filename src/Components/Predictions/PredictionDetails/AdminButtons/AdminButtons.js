d96fe033bfd4bea8dc99ee16a330f88572b1c9a2
Drinoff <drinoff@gmail.com>
2021-12-03 19:03:41 +0100

    Admin Buttons added

---
+++ src/Components/Predictions/PredictionDetails/AdminButtons/AdminButtons.js
@@ -0,0 +1,13 @@
+import './AdminButtons.css';
+
+
+const AdminButtons = () => {
+    return (
+    <div className="adminButtonsWrapper">
+        <button className = 'adminButtonsStyle'>Edit</button>
+        <button className = 'adminButtonsStyle'>Delete</button>
+    </div>
+    )
+}
+
+export default AdminButtons;
