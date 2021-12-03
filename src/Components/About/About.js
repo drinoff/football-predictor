97de3c6babfe7f3107fc902fd0ada5b12c74b815
Drinoff <drinoff@gmail.com>
2021-12-03 22:16:42 +0100

    About Component restyled

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,13 +6,13 @@ const About = () => {
             <h1>Football Predictor</h1>
             <p>
                 We are a football enthusiasts, who mainly watch football in our
-                Free time. We love football, we dream about football, and of
-                course we decided why not materialize our passion. We decided to
-                create a platform where we can share our passion with the
+                <span className = 'purple'>Free time</span>. We love <span className = 'green'>football</span>, we dream about football, and of
+                course we decided why not <span className = 'purple'>materialize</span> our passion. We decided to
+                create a platform where we can share our <span className = 'purple'>passion</span> with the
                 world.We created this platform to help you find the best
                 football bets and stats. With our platform you can share your bets as well.
 
-                This platform is made purely for fun. Please be careful while betting, since it can be addictive.
+                This platform is made purely for <span className = 'green'>fun</span>. Please be careful while betting, since it can be addictive.
                 Betting is a serious business, and we are not responsible for any losses.
                 We hope you enjoy our platform.
                 
