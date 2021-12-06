525a5c3b3493dbdd4bfeb335f1001e3c80cb9f14
Drinoff <drinoff@gmail.com>
2021-12-06 21:40:40 +0100

    edit and delete buttons created

--- src/Components/Predictions/PredictionDetails/AdminButtons/AdminButtons.js
+++ src/Components/Predictions/PredictionDetails/AdminButtons/AdminButtons.js
@@ -1,11 +1,21 @@
+import {useNavigate} from 'react-router-dom';
+
 import './AdminButtons.css';
 
 
-const AdminButtons = () => {
+const AdminButtons = ({id, urlMatchInfo}) => {
+    const navigate = useNavigate();
+    const onEditButtonClickHandler = () => {
+        navigate( `/predictions/${urlMatchInfo}/edit` , {state: {id}});
+    };
+
+    const onDeleteButtonClickHandler = () => {
+        console.log(id)
+    };
     return (
     <div className="adminButtonsWrapper">
-        <button className = 'adminButtonsStyle'>Edit</button>
-        <button className = 'adminButtonsStyle'>Delete</button>
+        <button className = 'adminButtonsStyle' onClick = {onEditButtonClickHandler}>Edit</button>
+        <button className = 'adminButtonsStyle' onClick = {onDeleteButtonClickHandler}>Delete</button>
     </div>
     )
 }
