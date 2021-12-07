014e9f7436e674481694b225c41de5924e23fdea
Drinoff <drinoff@gmail.com>
2021-12-07 20:45:41 +0100

    added input fields for EditPrediction Component

--- src/Components/Predictions/EditPrediction/EditPrediction.js
+++ src/Components/Predictions/EditPrediction/EditPrediction.js
@@ -1,24 +1,44 @@
-import { useEffect} from 'react';
-import { useLocation } from 'react-router';
+import { useEffect } from "react";
+import { useLocation } from "react-router";
 
-import predictionServices from '../../../services/predictionServices';
+import predictionServices from "../../../services/predictionServices";
 
 const Editprediction = () => {
     const location = useLocation();
     const id = location.state.id;
-    console.log(id)
+    console.log(id);
 
     useEffect(() => {
-        predictionServices.getPredictionById(id)
-        .then(data => console.log(data))
+        predictionServices
+            .getPredictionById(id)
+            .then((data) => console.log(data));
     }, [id]);
     return (
-        <div className="edintFormContainer">
-            <form id = 'edit-form' action="PUT"></form>
-            <label htmlFor="match" className="match"></label>
-            <input type="text"  />
+        <div className="editFormContainer">
+            <form id="edit-form" action="PUT">
+
+                <div className="editMatchName">
+                    <label htmlFor="match" className="match"></label>
+                    <input type="text" />
+                </div>
+
+                <div className="editPrediction">
+                    <label htmlFor="prediction" className="prediction">
+                        Prediction
+                    </label>
+                    <input type="text" />
+                </div>
+
+                <div className="editDescription">
+                    <label htmlFor="description" className="description">
+                        Description
+                    </label>
+                    <textarea type="text"></textarea>
+                </div>
+                
+            </form>
         </div>
-    )
+    );
 };
 
 export default Editprediction;
