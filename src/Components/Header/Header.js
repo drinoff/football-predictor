7159770629c91d7474f280542df0d45467837ace
Drinoff <drinoff@gmail.com>
2021-12-07 20:44:42 +0100

    logo added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,6 +4,7 @@ import AuthContext from "../../contexts/AuthContext";
 import NavBar from "./NavBar/NavBar";
 import User from "../User/User";
 import "./Header.css";
+import logo from "../../Assets/images/FP.svg";
 
 const Header = () => {
     const { user } = useContext(AuthContext);
@@ -11,16 +12,19 @@ const Header = () => {
     return (
         <div className="header">
             {user.isAuthenticated ? (
-                <User 
-                email={user.email}
-                 />
+                <User email={user.email} />
             ) : (
                 <div className="guestWelcome">
                     <p> Welcome Guest</p>
                     <p> Please Register or Sign In</p>
                 </div>
             )}
-            <NavBar isAuthenticated = {user.isAuthenticated}/>
+
+            <i className="fas fa-circle"></i>
+            <div className="logoContainer">
+                <img src={logo} alt="alt-logo" />
+            </div>
+            <NavBar isAuthenticated={user.isAuthenticated} />
         </div>
     );
 };
