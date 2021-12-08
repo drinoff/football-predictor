972e603e496239cc28cc37f76368fe00c7aac501
Drinoff <drinoff@gmail.com>
2021-12-08 20:39:03 +0100

    added editPrediction,deletePrediction and updateLikes to predictionServices

--- src/services/predictionServices.js
+++ src/services/predictionServices.js
@@ -1,35 +1,50 @@
-
-const BASE_URL = "https://football-predictor-3416d-default-rtdb.europe-west1.firebasedatabase.app/";
+const BASE_URL =
+    "https://football-predictor-3416d-default-rtdb.europe-west1.firebasedatabase.app/";
 const postPrediction = (body) => {
-    fetch(`${BASE_URL}predictions.json`,{
-        method: 'POST',
+    fetch(`${BASE_URL}predictions.json`, {
+        method: "POST",
         body: JSON.stringify(body),
-    
-    })
-    
-}
+    });
+};
 const getPredictions = () => {
-     return fetch(`${BASE_URL}predictions.json`)
-                .then(response => response.json())  
-}
+    return fetch(`${BASE_URL}predictions.json`).then((response) =>
+        response.json()
+    );
+};
 
 const getPredictionById = (id) => {
-    return fetch(`${BASE_URL}predictions/${id}.json`)
-                .then(response => response.json())  
-}
+    return fetch(`${BASE_URL}predictions/${id}.json`).then((response) =>
+        response.json()
+    );
+};
 
-const editMatch = (id,body) => {
-    return fetch(`${BASE_URL}predictions/${id}.json`,{
-        method: 'PUT',
+const editPrediction = (id, body) => {
+    return fetch(`${BASE_URL}predictions/${id}.json`, {
+        method: "PUT",
         body: JSON.stringify(body),
-    }
-    )}
+    }).then((res) => res.json());
+};
+
+const deletePrediction = (id) => {
+    return fetch(`${BASE_URL}predictions/${id}.json`, {
+        method: "DELETE",
+    }).then((res) => res.json());
+};
+
+const updateLikes = (id, body) => {
+    return fetch(`${BASE_URL}predictions/${id}.json`, {
+        method: "PUT",
+        body: JSON.stringify(body),
+    }).then((res) => res.json());
+};
 
 const predictionServices = {
     postPrediction,
     getPredictions,
     getPredictionById,
-    editMatch
-}
+    editPrediction,
+    deletePrediction,
+    updateLikes
+};
 
 export default predictionServices;
