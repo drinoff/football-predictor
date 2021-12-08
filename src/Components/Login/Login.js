2f94c3693a4c9c8dbe0e31cdf9e411815f8edf04
Drinoff <drinoff@gmail.com>
2021-12-08 20:41:40 +0100

    added Modal popup on login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,13 +1,17 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router";
 
 import { Box } from "@mui/system";
+import BasicModal from "../BasicModal/BasicModal";
 
 import AuthContext from "../../contexts/AuthContext";
 import authServices from "../../services/authServices";
 import "./Login.css";
 
 const Login = () => {
+    const [openModal, setOpenModal] = useState(false);
+    const [email, setEmail] = useState("");
+    const [error, setError] = useState();
     const { login } = useContext(AuthContext);
     const navigate = useNavigate();
 
@@ -18,22 +22,43 @@ const Login = () => {
         const password = formData.get("password");
 
         authServices.login(email, password).then((authData) => {
-            console.log(authData)
             const user = {
                 email: authData.user.email,
                 accessToken: authData.user.accessToken,
                 uid: authData.user.uid,
                 isAuthenticated: true,
-            };
+            }
+
+            setEmail(user.email);
             login(user);
-            navigate("/");
-        });
+            setOpenModal(true);
+            setTimeout(() => {
+                navigate("/");
+            }, 2000);
+
+            }).catch((err) => {
+                setError(err.message);
+                setOpenModal(true);
+                
+            });
+            setTimeout(() => {
+                navigate('/login')
+            }, 2000);
+            
+            
+        
     };
     return (
         <Box
             className="loginContainer"
             sx={{ bgcolor: "#111827", height: "80%", width: "33%" }}
         >
+            <BasicModal
+                openModal={openModal}
+                msg={"Success"}
+                secondMsg={`Hello ${email}`}
+                errorMsg={error}
+            />
             <h1 className="pleaseLogin">Please Login</h1>
             <form
                 id="login-form"
