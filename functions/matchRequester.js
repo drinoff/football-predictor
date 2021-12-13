7c91df67ad7e00cc63c0cb0b87d7d952f24807cd
Drinoff <drinoff@gmail.com>
2021-12-13 19:50:50 +0100

    update to matchRequester netlify function

--- functions/matchRequester.js
+++ functions/matchRequester.js
@@ -1,12 +1,14 @@
 const fetch = require("node-fetch");
-const API_KEY = process.env.API_FOOTBAL_API_KEY;
+const API_KEY = process.env.API_FOOTBALL_API_KEY;
 const API_ENDPOINT = "https://v3.football.api-sports.io/";
 
 exports.handler = async (event, context) => {
     const path = event.path;
 
     return fetch(API_ENDPOINT + path, {
+        headers: {
         "x-apisports-key": API_KEY,
+        }
     })
         .then((res) => res.json())
         .then((data) => {
