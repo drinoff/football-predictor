788ddb4be90a3aeb9c8598384da2c967aa3c45f0
Mehmet Ismail <drinoff@gmail.com>
2021-12-18 14:30:04 +0100

    removed underline text decoration from register and login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -10,94 +10,92 @@ import authServices from "../../services/authServices";
 import "./Login.css";
 
 const Login = () => {
-    const [openModal, setOpenModal] = useState(false);
-    const [email, setEmail] = useState("");
-    const [error, setError] = useState();
-    const { login } = useContext(AuthContext);
-    const navigate = useNavigate();
+  const [openModal, setOpenModal] = useState(false);
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState();
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    const onSubmitHandler = (e) => {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const email = formData.get("email");
-        const password = formData.get("password");
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const email = formData.get("email");
+    const password = formData.get("password");
 
-        authServices
-            .login(email, password)
-            .then((authData) => {
-                const user = {
-                    email: authData.user.email,
-                    accessToken: authData.user.accessToken,
-                    uid: authData.user.uid,
-                    isAuthenticated: true,
-                };
+    authServices
+      .login(email, password)
+      .then((authData) => {
+        const user = {
+          email: authData.user.email,
+          accessToken: authData.user.accessToken,
+          uid: authData.user.uid,
+          isAuthenticated: true,
+        };
 
-                setEmail(user.email);
-                login(user);
-                setError(null);
-                setOpenModal(true);
-                setTimeout(() => {
-                    setOpenModal(false);
-                    navigate("/");
-                }, 2000);
-            })
-            .catch((err) => {
-                setError("Invalid Email or Password");
-                setOpenModal(true);
-                setOpenModal(false);
-                navigate("/login");
-            });
-    };
-    return (
-        <Box
-            className="loginContainer"
-            sx={{ bgcolor: "#111827", height: "80%", width: "33%" }}
-        >
-            <BasicModal
-                openModal={openModal}
-                msg={"Success"}
-                secondMsg={`Hello ${email}`}
-                errorMsg={error}
-            />
-            <h1 className="pleaseLogin">Please Login</h1>
-            <form
-                id="login-form"
-                action="POST"
-                className="login-form"
-                onSubmit={onSubmitHandler}
-            >
-                <div className="emailContainer">
-                    <label htmlFor="email">Email</label>
-                    <input
-                        id="email"
-                        type="text"
-                        name="email"
-                        className="loginFormInputStyle"
-                    />
-                </div>
+        setEmail(user.email);
+        login(user);
+        setError(null);
+        setOpenModal(true);
+        setTimeout(() => {
+          setOpenModal(false);
+          navigate("/");
+        }, 2000);
+      })
+      .catch((err) => {
+        setError("Invalid Email or Password");
+        setOpenModal(true);
+        setOpenModal(false);
+        navigate("/login");
+      });
+  };
+  return (
+    <Box
+      className="loginContainer"
+      sx={{ bgcolor: "#111827", height: "80%", width: "33%" }}
+    >
+      <BasicModal
+        openModal={openModal}
+        msg={"Success"}
+        secondMsg={`Hello ${email}`}
+        errorMsg={error}
+      />
+      <h1 className="pleaseLogin">Please Login</h1>
+      <form
+        id="login-form"
+        action="POST"
+        className="login-form"
+        onSubmit={onSubmitHandler}
+      >
+        <div className="emailContainer">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="text"
+            name="email"
+            className="loginFormInputStyle"
+          />
+        </div>
 
-                <div className="passwordContainer">
-                    <label htmlFor="password">Password</label>
-                    <input
-                        id="password"
-                        type="password"
-                        name="password"
-                        className="loginFormInputStyle"
-                    />
-                </div>
+        <div className="passwordContainer">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            className="loginFormInputStyle"
+          />
+        </div>
 
-                <input className="loginButton" type="submit" value="Login" />
-                <p className="dontHaveAnAcc">
-                    Don't have an account?{" "}
-                    <Link to="/register">
-                        <span className="registerLinkinLoginComp">
-                            Register
-                        </span>
-                    </Link>
-                </p>
-            </form>
-        </Box>
-    );
+        <input className="loginButton" type="submit" value="Login" />
+        <p className="dontHaveAnAcc">
+          Don't have an account?{" "}
+          <Link to="/register" className="registerLinkinLoginComp">
+            <span>Register</span>
+          </Link>
+        </p>
+      </form>
+    </Box>
+  );
 };
 
 export default Login;
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -10,115 +10,116 @@ import authServices from "../../services/authServices";
 import "./Register.css";
 
 const Register = (props) => {
-    const { register } = useContext(AuthContext);
-    const [openModal, setOpenModal] = useState(false);
-    const [error, setError] = useState();
-    const navigate = useNavigate();
+  const { register } = useContext(AuthContext);
+  const [openModal, setOpenModal] = useState(false);
+  const [error, setError] = useState();
+  const navigate = useNavigate();
 
-    const onSubmitHandler = (e) => {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const email = formData.get("email");
-        const password = formData.get("password");
-        const rePass = formData.get("rePass");
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const email = formData.get("email");
+    const password = formData.get("password");
+    const rePass = formData.get("rePass");
 
-        if (password !== rePass) {
-            setError("Passwords do not match");
-            setOpenModal(true);
-            setTimeout(() => {
-                setOpenModal(false);
-                navigate("/register");
-            }, 1000);
-        } else if (password.length < 6) {
-            setError("Password must be at least 6 characters");
-            setOpenModal(true);
-            setTimeout(() => {
-                setOpenModal(false);
-                navigate("/register");
-            }, 1000);
-        } else {
-            authServices
-                .register(email, password)
-                .then((authData) => {
-                    const user = {
-                        email: authData.user.email,
-                        accessToken: authData.user.accessToken,
-                        uid: authData.user.uid,
-                        isAuthenticated: true,
-                    };
-                    register(user);
-                    setError(null);
-                    setOpenModal(true);
-                    setTimeout(() => {
-                        navigate("/");
-                    }, 2000);
-                })
-                .catch((err) => {
-                    setError(err.message);
-                    setOpenModal(true);
-                    setTimeout(() => {
-                        navigate("/register");
-                        setOpenModal(false);
-                    }, 1000);
-                });
-        }
-    };
-    return (
-        <Box
-            className="registerContainer"
-            sx={{ bgcolor: "#111827", height: "80%", width: "33%" }}
-        >
-            <BasicModal
-                openModal={openModal}
-                msg={"Success"}
-                secondMsg={"You have successfully registered"}
-                errorMsg={error}
-            />
-            <h1 className="pleaseRegister">Please Register</h1>
-            <form
-                id="register-form"
-                action="POST"
-                className="register-form"
-                onSubmit={onSubmitHandler}
-            >
-                <div className="emailContainer">
-                    <label htmlFor="email">Email</label>
-                    <input
-                        id="email"
-                        type="text"
-                        name="email"
-                        className="registerFormInputStyle"
-                    />
-                </div>
+    if (password !== rePass) {
+      setError("Passwords do not match");
+      setOpenModal(true);
+      setTimeout(() => {
+        setOpenModal(false);
+        navigate("/register");
+      }, 1000);
+    } else if (password.length < 6) {
+      setError("Password must be at least 6 characters");
+      setOpenModal(true);
+      setTimeout(() => {
+        setOpenModal(false);
+        navigate("/register");
+      }, 1000);
+    } else {
+      authServices
+        .register(email, password)
+        .then((authData) => {
+          const user = {
+            email: authData.user.email,
+            accessToken: authData.user.accessToken,
+            uid: authData.user.uid,
+            isAuthenticated: true,
+          };
+          register(user);
+          setError(null);
+          setOpenModal(true);
+          setTimeout(() => {
+            navigate("/");
+          }, 2000);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setOpenModal(true);
+          setTimeout(() => {
+            navigate("/register");
+            setOpenModal(false);
+          }, 1000);
+        });
+    }
+  };
+  return (
+    <Box
+      className="registerContainer"
+      sx={{ bgcolor: "#111827", height: "80%", width: "33%" }}
+    >
+      <BasicModal
+        openModal={openModal}
+        msg={"Success"}
+        secondMsg={"You have successfully registered"}
+        errorMsg={error}
+      />
+      <h1 className="pleaseRegister">Please Register</h1>
+      <form
+        id="register-form"
+        action="POST"
+        className="register-form"
+        onSubmit={onSubmitHandler}
+      >
+        <div className="emailContainer">
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="text"
+            name="email"
+            className="registerFormInputStyle"
+          />
+        </div>
 
-                <div className="passwordContainer">
-                    <label htmlFor="password">Password</label>
-                    <input
-                        id="password"
-                        type="password"
-                        name="password"
-                        className="registerFormInputStyle"
-                    />
-                </div>
-                <div className="rePassContainer">
-                    <label htmlFor="rePass">Password</label>
-                    <input
-                        id="rePass"
-                        type="password"
-                        name="rePass"
-                        className="registerFormInputStyle"
-                    />
-                </div>
+        <div className="passwordContainer">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            className="registerFormInputStyle"
+          />
+        </div>
+        <div className="rePassContainer">
+          <label htmlFor="rePass">Password</label>
+          <input
+            id="rePass"
+            type="password"
+            name="rePass"
+            className="registerFormInputStyle"
+          />
+        </div>
 
-                <input
-                    className="registerButton"
-                    type="submit"
-                    value="Register"
-                />
-                <p className="alreadyGotAcc">Don't have an account? <Link to='/login' ><span className="loginLinkInRegisterComp">Login</span></Link></p>
-            </form>
-        </Box>
-    );
+        <input className="registerButton" type="submit" value="Register" />
+        <p className="alreadyGotAcc">
+          Don't have an account?{" "}
+          <Link to="/login" className="loginLinkInRegisterComp">
+            <span>Login</span>
+          </Link>
+        </p>
+      </form>
+    </Box>
+  );
 };
 
 export default Register;
